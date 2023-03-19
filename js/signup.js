b4285d0998970fba6ba9646b8cdd8680bd256889
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-19 21:22:58 +0800

    when the

--- js/signup.js
+++ js/signup.js
@@ -3,12 +3,13 @@
 // and the cookie should say that user is already signed up
 var submitButton = document.getElementById("submitter")
 
-var inputElements = document.querySelectorAll("#signupform input")
+var inputElements = document.querySelectorAll("#signupform input, select, ")
+
 function logger() {
     for (el of inputElements) {
         if (el.value == "Submit") continue
         setCookie(el.name, el.value, 365)
     }
     setCookie("signedIn", 1, 365)
-    location.href="./"
+    location.href = "./"
 }

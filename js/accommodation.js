ef2dcfb2b33dc501040e3783e4e5ec5c7c7715c5
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-21 20:44:46 +0800

    bug fix by manually making photo blurred; thanks gdrive hotlinks

--- js/accommodation.js
+++ js/accommodation.js
@@ -1,29 +1,23 @@
 for (els of document.getElementsByClassName("accobook")) {
     els.onclick = function(e) {
-        var modal = document.getElementById("myModal");
-        modal.style.display = "block";
-        modal.style.position = "fixed";
+
         if (hasTagContext(e.target)) {
-            alert(getTagContext(e.target))
+            //alert(getTagContext(e.target))
+            header.innerText = `Booking a ${getTagContext(e.target).toProperCase()} room`
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+            modal.style.position = "fixed";
+
         }
     }
 }
-
-// Get the modal
+var header = document.getElementById("headermod")
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
--- js/bloat.js
+++ js/bloat.js
@@ -30,3 +30,6 @@ function getTagContext(element) {
   return element.getAttribute("data")
 }
 
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};

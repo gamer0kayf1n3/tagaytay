95e521d07ae22459718d3df5be2e3897aa9236da
BermudaVI <95553053+BermudaVI@users.noreply.github.com>
2023-03-22 10:41:34 +0800

    limit start date to today and end date to after start date

--- js/accommodation.js
+++ js/accommodation.js
@@ -1,7 +1,12 @@
+
 for (els of document.getElementsByClassName("accobook")) {
     els.onclick = function(e) {
-
+      document.getElementById("before").value=""
+      document.getElementById("after").value=""
         if (hasTagContext(e.target)) {
+          document.getElementById("before").min = new Date().toISOString().split("T")[0];
+          
+          
             //alert(getTagContext(e.target))
             header.innerText = `Booking a ${getTagContext(e.target).toProperCase()} room`
             var modal = document.getElementById("myModal");
@@ -17,7 +22,10 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
-
+document.getElementById("before").addEventListener("input", () => {
+  console.log("wow")
+  document.getElementById("after").min = new Date(document.getElementById("before").value).toISOString().split("T")[0];
+})
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";

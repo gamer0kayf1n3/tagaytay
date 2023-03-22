65bcbb832f5e300d9687d035dcb73e6606cf99f5
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-22 19:35:09 +0800

    added bloat createTable()

--- js/bloat.js
+++ js/bloat.js
@@ -33,3 +33,24 @@ function getTagContext(element) {
 String.prototype.toProperCase = function () {
   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
+
+
+function createTable(tableData) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}

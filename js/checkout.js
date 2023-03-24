45177989f16f7f6da6162a177cd693f386316645
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-24 21:54:24 +0800

    dont print this

--- js/checkout.js
+++ js/checkout.js
@@ -14,44 +14,44 @@ receipt = document.getElementsByClassName("receipt")[0]
 var data = JSON.parse(getCookie("cartData"))
 
 pricing = {
-    "single":700.00,
-    "double":800.00,
-    "triple":900.00,
-    "quad":1000.00,
-    "family":1200.00
-  }
-altnames = {
-    "single":"Standard Single",
-    "double":"Standard Double",
-    "triple":"Standard Triple",
-    "quad":"Standard Quad",
-    "family":"Deluxe Family"
+    "single": 700.00,
+    "double": 800.00,
+    "triple": 900.00,
+    "quad": 1000.00,
+    "family": 1200.00
 }
-/*
-//roomtype
-altnames[datas[0]]
-//roomrate
-pricing[datas[0]]
-//checkin date
-new Date(datas[1])
-//checkoutdate
-new Date(datas[2])
-//day numbers
-(out - in)/1000/86400
-//sub
-day * roomrate
-*/
+altnames = {
+        "single": "Standard Single",
+        "double": "Standard Double",
+        "triple": "Standard Triple",
+        "quad": "Standard Quad",
+        "family": "Deluxe Family"
+    }
+    /*
+    //roomtype
+    altnames[datas[0]]
+    //roomrate
+    pricing[datas[0]]
+    //checkin date
+    new Date(datas[1])
+    //checkoutdate
+    new Date(datas[2])
+    //day numbers
+    (out - in)/1000/86400
+    //sub
+    day * roomrate
+    */
 
-for (datas of data) 
-{   datas = datas.split(" ")
+for (datas of data) {
+    datas = datas.split(" ")
     roomtype = altnames[datas[0]]
     roomrate = pricing[datas[0]]
     checkind = new Date(datas[1])
     checkout = new Date(datas[2])
-    daynumbs = (checkout - checkind)/1000/86400
+    daynumbs = (checkout - checkind) / 1000 / 86400
     subtotal = daynumbs * roomrate
     receipt.insertAdjacentHTML("beforeend",
-        `<div class="room-info">
+            `<div class="room-info">
         <strong>Room Type:</strong><span class="right"> ${roomtype}</span><br>
         <strong>Room Rate:</strong><span class="right"> ${`PHP ${(roomrate).toLocaleString()}.00`}</span><br>
         <strong>Check-in Date:</strong><span class="right"> ${checkind.toLocaleString('default', {weekday:"long", month: 'long', day:"numeric",year:"numeric"})}</span><br>

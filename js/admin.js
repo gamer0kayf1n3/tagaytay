d40df4e7eb5efd266d1a2d5a8ea0d204b2b629e6
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-25 13:27:20 +0800

    time + sidebar

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,27 @@
 setTimeout(()=> {
     document.getElementById("intro").remove()
 },2400)
+
+dashboarbutton = document.getElementById("dashboard")
+dashboard = document.getElementById("sidebar")
+exitButton = document.getElementById("bye")
+timeEl = document.getElementById("current-time")
+dashboarbutton.onclick = function() {
+    dashboard.style.display = "block"
+    dashboard.style.animation = "slidein 200ms ease forwards"
+}
+exitButton.onclick = function() {
+    dashboard.style.display = "none"
+}
+
+function getTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+  
+  setInterval(() => {
+    const currentTime = getTime();
+    timeEl.innerText = currentTime
+  }, 1000);

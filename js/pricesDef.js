6a77e09415d40944d776bd2e18b70bcefbca486a
BermudaVI <95553053+BermudaVI@users.noreply.github.com>
2023-03-28 20:06:14 +0800

    Prices
    yung js nalang i rename

--- js/pricesDef.js
+++ js/pricesDef.js
@@ -1,7 +1,21 @@
 var prices = {
-    "single":2000,
-    "double":2200,
-    "triple":7392,
-    "quad":2600,
-    "family":2800
+    "single":3799,
+    "double":4899,
+    "triple":7299,
+    "quad":8799,
+    "family":9999
 }
+
+/* Steven pls fix ctrl shif h on me is destructive
+var newprices = {
+    "stdqueen":3799,
+    "stdking":4899,
+    "luxtwin":7299,
+    "luxdobqueen":8799,
+    "luxdobking":9999
+}
+
+prices are based on multiple sources, the stdqueen price was based
+on my previous hotel experience costing 6k+ with 2 bedrooms with
+queen beds
+*/

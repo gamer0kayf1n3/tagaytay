68a48c95a48d1a07ee73ee5b948552f58125a5a9
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-03-28 20:10:44 +0800

    no comment

--- js/pricesDef.js
+++ js/pricesDef.js
@@ -5,17 +5,3 @@ var prices = {
     "quad":8799,
     "family":9999
 }
-
-/* Steven pls fix ctrl shif h on me is destructive
-var newprices = {
-    "stdqueen":3799,
-    "stdking":4899,
-    "luxtwin":7299,
-    "luxdobqueen":8799,
-    "luxdobking":9999
-}
-
-prices are based on multiple sources, the stdqueen price was based
-on my previous hotel experience costing 6k+ with 2 bedrooms with
-queen beds
-*/
